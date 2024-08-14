621c3b0bfd3d13c5eff1ef5368215f621a29828b
moumit-b <moumitb@bu.edu>
2024-08-14 00:26:19 -0400

    updated server.js for mongodb

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const firebaseAdmin = require('./config/firebaseConfig');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('MongoDB connection error:', err));
 
 const app = express();
 app.use(express.json());
