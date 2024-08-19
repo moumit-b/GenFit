ff548c5a6dcffadec1fa3249fb305cd6aefb9547
moumit-b <moumitb@bu.edu>
2024-08-18 22:16:45 -0400

    Added authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const firebaseAdmin = require('../config/firebaseConfig');
+
+const authenticate = async (req, res, next) => {
+    const authHeader = req.headers.authorizationl;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'No token provided'});
+    }
+    
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+        req.user = decodedToken; 
+        next();
+    
+    } catch (error){
+        res.status(401).json({message:'Unauthorized'});
+    }
+    
+};
+
+module.exports = authenticate;
+
