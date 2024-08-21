a33d7c2502c51ca4db522208e9686c3f34216876
moumit-b <moumitb@bu.edu>
2024-08-21 00:01:37 -0400

    updated userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticate = require('../middleware/authMiddleware');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.put('/update/:id', userController.updateUser);
-router.get('/:id', userController.getUser);
+router.put('/update/:id', authenticate, userController.updateUser);
+router.get('/:id', authenticate, userController.getUser);
 
 module.exports = router;
